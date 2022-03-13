37d64f0059d64d63c6ecd91d5a28f16c9f649286
Nartallax <nartallax@gmail.com>
2022-03-13 23:34:51 +0300

    1.0.19: removed reference to @types/node from .d.ts

--- scripts/generate_dts.sh
+++ scripts/generate_dts.sh
@@ -5,5 +5,8 @@ cd ..
 
 TMP_DTS_FILE="$2.tmp"
 ./node_modules/.bin/dts-bundle-generator -o "$2" --project tsconfig.json --no-banner "$1"
-sed 's/export [*] from.*//g' "$2" | sed 's/export [{][}].*//g' > $TMP_DTS_FILE
+# note that we explicitly dropping @types/node here; we don't really need them in our typings
+# dts-bundle-generator for some reason thinks that we use RelativeIndexable<T>
+sed 's/export [*] from.*//g' "$2" | sed 's/export [{][}].*//g' | sed 's/\/\/\/ <reference types="node" \/>/ /' > "$TMP_DTS_FILE"
+
 mv $TMP_DTS_FILE "$2"
